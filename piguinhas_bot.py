6f97a3024ab4329a61eaabd2fb7e014ba32f19eb
GiribolaH <henrique.giribola@gmail.com>
2023-07-09 00:44:09 -0300

    Para de ler a própria mensagem

--- piguinhas_bot.py
+++ piguinhas_bot.py
@@ -1,35 +1,33 @@
 import discord
 from discord.ext import commands
 
-#BARD#
-#from bardapi import Bard
-#import os
-#os.environ["_BARD_API_KEY"] = {
+# BARD #
+# from bardapi import Bard
+# import os
+# os.environ["_BARD_API_KEY"] = {
 #    'Xwj2TA6QSk8csb0uecrbp2LKzTUa_mrW-KzWSieYVEo0Aze1lZDLRhO8-cjL8sOLu2cV1g.'
-#}
-#pergunta = input("")
-#reposta bot:
-#print(Bard.getanswer(str(message)))#
+# }
+# pergunta = input("")
+# reposta bot:
+# print(Bard.getanswer(str(message)))#
 
 prefix = "/"
 intent = discord.Intents.default()
 bot = commands.Bot(command_prefix=prefix, intents=intent, help_command=None)
 
+
 @bot.event
 async def on_ready():
     print(f"{bot.user.name}: Loaded")
 
-perguntas = {}
-pergunta = input("")
-perguntas.append(pergunta)
-aux = len(perguntas)
 
-while aux > 0:
-    @bot.event
-    async def on_message(message):
-        print(f"Bot envia: {repr(message.content)}")
-        await bot.process_commands(message)
-        await message.channel.send("ping")
-    aux - 1
+@bot.event
+async def on_message(message):
 
-bot.run("MTEwNzAwNzk5MjM4NzM0MjQwNw.GLP0aY.RUqpefuvyyHSZn_n1vCShfNYdJF6E6Rinhcuus")
+    if message.author == bot.user:
+        return
+
+    await bot.process_commands(message)
+    await message.channel.send("ping")
+
+bot.run('MTEwNzAwNzk5MjM4NzM0MjQwNw.GHoia9.AABBgyD9zYkZQ2sFv_2EqECz31CFFUIjzGD8Sk')
